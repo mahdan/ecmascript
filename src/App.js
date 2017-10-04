5a9a3122caceb83753bd7052c47089ea72f5818a
Mandip Ahdan <Mandip@Users-MacBook.local>
2017-10-04 20:56:08 +0100

    es6 functions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
+    const PI = 3.141593;
+    let myName = "Mandip";
+    let circleArea = (pi, r) => pi * r * r;
+    let result = circleArea(3.14, 3);
+
+    console.log(result);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="container">
+        <header>
+          <h1>ES6 and beyond feature demo</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <h2>Const</h2>
+        <p>The new const keyword makes it possible to declare constants, also known as immutable variables, to which we cannot reassign new content later.</p>
+        <p>
+          PI = {PI}
         </p>
+        <h2>Let</h2>
+        <p>Block-scoped binding constructs. let is the new var</p>
+        <p>my name is {myName}</p>
+        <h2>Arrow function</h2>
+        <p>Arrows are a function shorthand using the => syntax.</p>
+        <p>e.g.<br/>
+        let circleArea = (pi, r) => pi * r * r;<br/>
+        let result = circleArea(3.14, 3);</p>
+        <p>Now view the console log and you should see 28.259999999999998</p>
       </div>
     );
   }
